94e4a259091a642408daa2b713bba1d7f697d50c
Chirkov Slava <slavachirkov92@gmail.com>
2023-03-18 21:23:56 +0300

    edit theme

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import changeTheme from './modules/change-theme'
 import workers from './modules/workers'
 import validation from './modules/validation'
 import modal from './modules/modal'
-// import load from './modules/preloader'
 
 changeTheme()
 workers()
 validation()
 modal()
-// load()
--- src/modules/preloader.js
+++
@@ -1,9 +0,0 @@
-// const load = () => {
-// 	const preloader = document.getElementById('preloader');
-
-// 	preloader.classList.add('show');
-
-// 	setTimeout(() => preloader.classList.remove('show'), 3000);
-// }
-
-// export default load
